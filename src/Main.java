d091fb4485e9ed7e9b474138bc36666c1c59cf39
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-12 20:09:02 +0400

    Задача 5.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 4.");
-        int deliveryDistance = 95;
-        if (deliveryDistance <= 20) {
-            System.out.println("Потребуется дней: 1");
-        } else {
-            int AmountDays = (deliveryDistance - 20);
-            if ((AmountDays % 40 == 0)) {
-                int NewAmountDays = (AmountDays / 40) + 1;
-                System.out.println("Потребуется дней: " + NewAmountDays);
-            } else {
-                int NewAmountDays2 = (AmountDays / 40) + 2;
-                System.out.println("Потребуется дней: " + NewAmountDays2);
-            }
+        System.out.println("Задача 5.");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Сезон года - зима.");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Сезон года - весна.");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Сезон года - лето.");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Сезон года - осень.");
+                break;
+            default:
+                System.out.println("Месяца с таким номером не существует.");
         }
     }
 }
