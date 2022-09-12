aec14894467fca0de4915691a8b56fda94ed8a65
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-12 16:56:36 +0400

    Задача 3.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Задача 2.");
-        int clientOS = 1; // (0 — iOS, 1 — Android)
-        int clientDeviceYear = 2015;
-        if (clientOS == 1 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для Android по ссылке.");
-        } else if (clientOS == 1 && clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Установите версию приложения для iOS по ссылке.");
+        System.out.println("Задача 3.");
+        int year = 2000;
+        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
         } else {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            System.out.println(year + " год не является високосным");
         }
     }
 }
