ca4161a990647fb3a4f15473514b55e8e3816ffc
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-13 20:28:28 +0400

    Исправил

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 4.");
-        int deliveryDistance = 95;
-        if (deliveryDistance <= 20) {
+        int DeliveryDistance = 95;
+        if (DeliveryDistance <= 20) {
             System.out.println("Потребуется дней: 1");
         } else {
-            int AmountDays = (deliveryDistance - 20);
+            int AmountDays = (DeliveryDistance - 20);
             if ((AmountDays % 40 == 0)) {
                 int NewAmountDays = (AmountDays / 40) + 1;
                 System.out.println("Потребуется дней: " + NewAmountDays);
