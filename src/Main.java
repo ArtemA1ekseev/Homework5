b1d4d82cfe2fc0c4e0c5466e96a69575bb1101fa
ArtemA1ekseev <a1ekseevartem.ru@gmail.com>
2022-09-13 20:33:36 +0400

    Исправил.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Задача 4.");
         int DeliveryDistance = 95;
         if (DeliveryDistance <= 20) {
